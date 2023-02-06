f897f8ff7b53eaa01fae2720552384774de49dea
dwboucher <qjr9rz@virginia.edu>
2023-02-05 21:07:24 -0500

    Update testit.py
    Added test for new function

--- testit.py
+++ testit.py
@@ -31,7 +31,15 @@ def test_assert_bool():
     assert "Expected str but got" in str(exc_mssg)
     assert "bool" in str(exc_mssg)
 
+def test_square():
+    with pytest.raises(Exception) as exc_mssg:
+        sh.square_func(word)
+
+    assert "Expected int but got" in str(exc_mssg):
+    assert "str" in str(exc_mssg)
+
 test_multi_spaces()
 test_multi_lines()
 test_assert_int()
-test_assert_bool()  
+test_assert_bool() 
+test_square()
